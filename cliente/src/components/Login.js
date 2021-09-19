4fd4d7095f183d14315b7bf2b7c54f4df880c10a
Gerarditron <gerardo.jose2011@yahoo.es>
2021-09-19 01:33:55 -0600

    login designs

--- cliente/src/components/Login.js
+++ cliente/src/components/Login.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import './stylesheets/login.css';
+
+import login from './imgs/login.jpg';
+import logo from './imgs/logo.png';
+import error from './imgs/error.png';
 
 export const Login = () => {
+
+
     return (
-        <div>
-            <h1>Login</h1>
+        <>
+        <div className="container">
+            <div>
+                <img src={login} className="picture"/>
+            </div>
+            <div className="loginbox">
+                <div>
+                    <img src={logo} className="logo" />
+                </div>
+                <div className="titleLogin">
+                    <label>Cuerpo de Bomberos</label><br/>
+                    <label>Inicio de Sesion</label>
+                </div>
+                <div>
+                    <form className="formLogin">
+                        <div className="formLogin">
+                            <input type="text" id="user" name="user" placeholder="Nombre de Usuario" className="textbox"/>
+                            <input type="text" id="user" name="user" placeholder="Contraseña" className="textbox" />
+                        </div>
+                        <div className="errorLogin">
+                            <img src={error} className="errorIco"/>
+                            <label className="lberror">Se ha equivocado en escribir su usuario o contraseña</label>
+                        </div>
+                        <button type="button" id="btnLogin" name="btnLogin" className="btnLogin">Ingresar</button>
+                        <button type="button" id="btnForgot" name="btnForgot" className="btnForgot">¿Olvidó su contraseña?</button>
+                    </form>
+                </div>
+            </div>
         </div>
+        </>
     )
 }
