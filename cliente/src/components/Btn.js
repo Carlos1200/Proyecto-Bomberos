fc17b8daaf6831d55361af2106f73f95600f5d54
Carlos1200 <50189393+Carlos1200@users.noreply.github.com>
2021-10-04 20:56:36 -0600

    Solucionando errores

--- cliente/src/components/Btn.js
+++ cliente/src/components/Btn.js
@@ -3,22 +3,6 @@ import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 
-const Boton = styled.button`
-  width: 100%;
-  border: 0;
-  border-bottom: 2px solid rgba(255, 255, 255, 0.8);
-  background-color: transparent;
-  color: white;
-  font-size: 18px;
-  padding: 15px 0;
-  display: flex;
-  justify-content: space-evenly;
-
-  &:hover {
-    transform: scale(1.02);
-  }
-`;
-
 export const Btn = ({ titulo, icono, size, redirect,onpress }) => {
   return (
     <Link to={redirect} style={{ textDecoration: "none" }}>
@@ -29,3 +13,19 @@ export const Btn = ({ titulo, icono, size, redirect,onpress }) => {
     </Link>
   );
 };
+
+const Boton = styled.button`
+  width: 100%;
+  border: 0;
+  border-bottom: 2px solid rgba(255, 255, 255, 0.8);
+  background-color: transparent;
+  color: white;
+  font-size: 18px;
+  padding: 15px 0;
+  display: flex;
+  justify-content: space-around;
+
+  &:hover {
+    transform: scale(1.02);
+  }
+`;
