9278a3a25442c0f3186cc447743f3838797772b7
Fernando <fernando99funes@gmail.com>
2021-11-03 18:25:31 -0600

    Proyecto act. 03/11

--- servidor/vendor/composer/installed.php
+++ servidor/vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'bcdeba02b9c046b1cb2dd6846aa8c0015aa8ff60',
+        'reference' => '9cce24f8423f8510c50cd6230c33108a0f622865',
         'name' => 'cdavi/servidor',
         'dev' => true,
     ),
@@ -16,7 +16,7 @@
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'bcdeba02b9c046b1cb2dd6846aa8c0015aa8ff60',
+            'reference' => '9cce24f8423f8510c50cd6230c33108a0f622865',
             'dev_requirement' => false,
         ),
         'graham-campbell/result-type' => array(
